266e51bd773bd2dcbfbc992e818795df3fbe6793
intsuc <intsuc@gmail.com>
2021-09-30 15:59:37 +0900

    Implement

---
+++ InsideOut/Elaborate.lean
@@ -0,0 +1,134 @@
+import InsideOut.Syntax
+
+abbrev Ctx := Array (String × Typ)
+
+abbrev M := Except String
+
+mutual
+
+partial def checkOutsideIn (Γ : Ctx) : Exp → Typ → M Unit
+  | exp let x₁ ∷ t₂ ≔ e₃; e₄, t => do
+    checkOutsideIn Γ e₃ t₂
+    checkOutsideIn (Γ.push (x₁, t₂)) e₄ t
+
+  | exp abs x₁ ⇒ e₂, typ t₁ ⇒ t₂ => do
+    checkOutsideIn (Γ.push (x₁, t₁)) e₂ t₂
+
+  | exp iff e₁ then e₂ else e₃, t => do
+    checkOutsideIn Γ e₁ $ typ bool
+    checkOutsideIn Γ e₂ t
+    checkOutsideIn Γ e₃ t
+
+  | e, expected => do
+    let found ← inferOutsideIn Γ e
+    if expected == found then () else throw s!"type expected '{expected}'; found '{found}'"
+
+partial def inferOutsideIn (Γ : Ctx) : Exp → M Typ
+  | exp let x₁ ∷ t₂ ≔ e₃; e₄ => do
+    checkOutsideIn Γ e₃ t₂
+    inferOutsideIn (Γ.push (x₁, t₂)) e₄
+
+  | e@(exp #x₁) =>
+    match Γ.find? (·.1 == x₁) with
+    | some (_, t₁) => t₁
+    | none         => throw s!"unknown variable '{e}'"
+
+  | e@(exp abs x₁ ⇒ e₂) => do
+    let (t₂, Γ) ← inferInsideOut Γ e₂
+    match Γ.find? (·.1 == x₁) with
+    | some (_, t₁) => typ t₁ ⇒ t₂
+    | none         => throw s!"partially failed to infer '{e} ∷ ? ⇒ {t₂}'"
+
+  | exp e₁ ◁ e₂ => do
+    let t₁ ← inferOutsideIn Γ e₁
+    match t₁ with
+    | typ t₁₁ ⇒ t₁₂ =>
+      checkOutsideIn Γ e₂ t₁₁
+      t₁₂
+    | found => throw s!"type expected '_ ⇒ _'; found '{found}'"
+
+  | exp ff => typ bool
+
+  | exp tt => typ bool
+
+  | exp iff e₁ then e₂ else e₃ => do
+    checkOutsideIn Γ e₁ $ typ bool
+    let t₂ ← inferOutsideIn Γ e₂
+    checkOutsideIn Γ e₃ t₂
+    t₂
+
+  | exp e₁ ∷ t₂ => do
+    checkOutsideIn Γ e₁ t₂
+    t₂
+
+partial def checkInsideOut (Γ : Ctx) : Exp → Typ → M Ctx
+  | exp let x₁ ∷ t₂ ≔ e₃; e₄, t => do
+    let Γ ← checkInsideOut Γ e₃ t₂
+    checkInsideOut (Γ.push (x₁, t₂)) e₄ t
+
+  | exp #x₁, t => Γ.push (x₁, t)
+
+  | exp abs x₁ ⇒ e₂, typ t₁ ⇒ t₂ => do
+    checkInsideOut (Γ.push (x₁, t₁)) e₂ t₂
+
+  | exp iff e₁ then e₂ else e₃, t => do
+    let Γ ← checkInsideOut Γ e₁ $ typ bool
+    let Γ ← checkInsideOut Γ e₂ t
+    checkInsideOut Γ e₃ t
+
+  | e, expected => do
+    let (found, Γ) ← inferInsideOut Γ e
+    if expected == found then Γ else throw s!"type expected '{expected}'; found '{found}'"
+
+partial def inferInsideOut (Γ : Ctx) : Exp → M (Typ × Ctx)
+  | exp let x₁ ∷ t₂ ≔ e₃; e₄ => do
+    let Γ ← checkInsideOut Γ e₃ t₂
+    inferInsideOut (Γ.push (x₁, t₂)) e₄
+
+  | e@(exp #x₁) =>
+    match Γ.find? (·.1 == x₁) with
+    | some (_, t₁) => (t₁, Γ)
+    | none         => throw s!"failed to infer '{e}'"
+
+  | e@(exp abs x₁ ⇒ e₂) => do
+    let (t₂, Γ) ← inferInsideOut Γ e₂
+    match Γ.find? (·.1 == x₁) with
+    | some (_, t₁) => (typ t₁ ⇒ t₂, Γ)
+    | none         => throw s!"partially failed to infer '{e} ∷ ? ⇒ {t₂}'"
+
+  | exp e₁ ◁ e₂ => do
+    let (t₁, Γ) ← inferInsideOut Γ e₁
+    match t₁ with
+    | typ t₁₁ ⇒ t₁₂ =>
+      let Γ ← checkInsideOut Γ e₂ t₁₁
+      (t₁₂, Γ)
+    | found => throw s!"type expected '_ ⇒ _'; found '{found}'"
+
+  | exp ff => (typ bool, Γ)
+
+  | exp tt => (typ bool, Γ)
+
+  | exp iff e₁ then e₂ else e₃ => do
+    let Γ       ← checkInsideOut Γ e₁ $ typ bool
+    let (t₂, Γ) ← inferInsideOut Γ e₂
+    let Γ       ← checkInsideOut Γ e₃ t₂
+    (t₂, Γ)
+
+  | exp e₁ ∷ t₂ => do
+    let Γ ← checkInsideOut Γ e₁ t₂
+    (t₂, Γ)
+
+end
+
+#eval inferOutsideIn #[] $ exp ff
+#eval inferOutsideIn #[] $ exp abs "x" ⇒ #"x"
+#eval inferOutsideIn #[] $ exp abs "x" ⇒ ff
+#eval inferOutsideIn #[] $ exp
+  let "f" ∷ bool ⇒ bool ≔ abs "x" ⇒ #"x";
+  abs "x" ⇒ #"f" ◁ #"x"
+#eval inferOutsideIn #[] $ exp
+  let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
+  abs "x" ⇒ #"f" ◁ #"x"
+#eval inferOutsideIn #[] $ exp
+  let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"y";
+  abs "x" ⇒ #"f" ◁ #"x"
---
+++ InsideOut/Syntax.lean
@@ -0,0 +1,79 @@
+inductive Typ where
+  | func : Typ → Typ → Typ
+  | bool : Typ
+  deriving BEq
+
+inductive Exp where
+  | «let» : String → Typ → Exp → Exp → Exp
+  | var   : String → Exp
+  | abs   : String → Exp → Exp
+  | app   : Exp → Exp → Exp
+  | ff    : Exp
+  | tt    : Exp
+  | iff   : Exp → Exp → Exp → Exp
+  | anno  : Exp → Typ → Exp
+
+declare_syntax_cat typ
+
+declare_syntax_cat exp
+
+syntax typ " ⇒ " typ : typ
+syntax "bool"        : typ
+syntax "( " typ " )" : typ
+syntax term          : typ
+
+syntax "let " term " ∷ " typ " ≔ " exp " ; " exp : exp
+syntax:100 "# " term:100                         : exp
+syntax "abs " term " ⇒ " exp                     : exp
+syntax:65 exp:65 " ◁ " exp:66                    : exp
+syntax "ff"                                      : exp
+syntax "tt"                                      : exp
+syntax "iff " exp " then " exp " else " exp      : exp
+syntax exp " ∷ " typ                             : exp
+syntax "( " exp " )"                             : exp
+syntax term                                      : exp
+
+macro_rules
+  | `(typ| $t₁ ⇒ $t₂) => `(Typ.func $t₁ $t₂)
+  | `(typ| bool)      => `(Typ.bool)
+  | `(typ| ($t:typ))  => `(typ| $t)
+  | `(typ| $t:term)   => t
+
+macro_rules
+  | `(exp| let $x₁ ∷ $t₂ ≔ $e₃; $e₄)  => `(Exp.let $x₁ $t₂ $e₃ $e₄)
+  | `(exp| #$x₁)                      => `(Exp.var $x₁)
+  | `(exp| abs $x₁ ⇒ $e₂)             => `(Exp.abs $x₁ $e₂)
+  | `(exp| $e₁ ◁ $e₂)                 => `(Exp.app $e₁ $e₂)
+  | `(exp| ff)                        => `(Exp.ff)
+  | `(exp| tt)                        => `(Exp.tt)
+  | `(exp| iff $e₁ then $e₂ else $e₃) => `(Exp.iff $e₁ $e₂ $e₃)
+  | `(exp| $e₁ ∷ $t₂)                 => `(Exp.anno $e₁ $t₂)
+  | `(exp| ($e:exp))                  => `(exp| $e)
+  | `(exp| $e:term)                   => e
+
+macro "typ " t:typ : term => t
+
+macro "exp " e:exp : term => e
+
+private def paren (p₁ p₂ : Nat) (s : String) : String :=
+  if p₂ < p₁ then s!"({s})" else s
+
+instance : ToString Typ where
+  toString :=
+    let rec go (p : Nat) : Typ → String
+      | typ t₁ ⇒ t₂ => paren p 0 s!"{go 1 t₁} ⇒ {go 0 t₂}"
+      | typ bool    => "bool"
+    go 0
+
+instance : ToString Exp where
+  toString :=
+    let rec go (p : Nat) : Exp → String
+      | exp let x₁ ∷ t₂ ≔ e₃; e₄   => paren p 0 s!"let {x₁} ∷ {t₂} ≔ {go 0 e₃}; {go 0 e₄}"
+      | exp #x₁                    => s!"#{x₁}"
+      | exp abs x₁ ⇒ e₂            => paren p 0 s!"abs {x₁} ⇒ {go 0 e₂}"
+      | exp e₁ ◁ e₂                => paren p 2 s!"{go 2 e₁} ◁ {go 3 e₂}"
+      | exp ff                     => "ff"
+      | exp tt                     => "tt"
+      | exp iff e₁ then e₂ else e₃ => paren p 0 s!"iff {go 0 e₁} then {go 0 e₂} else {go 0 e₃}"
+      | exp e₁ ∷ t₂                => paren p 1 s!"{go 1 e₁} ∷ {t₂}"
+    go 0
