052ee83f5f9622dfd3c4bbfb8babf956824b08ca
intsuc <intsuc@gmail.com>
2021-10-01 17:51:58 +0900

    Use application context

--- InsideOut/Elaborate.lean
+++ InsideOut/Elaborate.lean
@@ -1,6 +1,7 @@
 import InsideOut.Syntax
 
-abbrev Ctx := Array (String × Typ)
+abbrev Ctx := List (String × Typ)
+abbrev App := List Typ
 
 abbrev M := Except String
 
@@ -9,10 +10,10 @@ mutual
 partial def checkOutsideIn (Γ : Ctx) : Exp → Typ → M Unit
   | exp let x₁ ∷ t₂ ≔ e₃; e₄, t => do
     checkOutsideIn Γ e₃ t₂
-    checkOutsideIn (Γ.push (x₁, t₂)) e₄ t
+    checkOutsideIn ((x₁, t₂) :: Γ) e₄ t
 
   | exp abs x₁ ⇒ e₂, typ t₁ ⇒ t₂ => do
-    checkOutsideIn (Γ.push (x₁, t₁)) e₂ t₂
+    checkOutsideIn ((x₁, t₁) :: Γ) e₂ t₂
 
   | exp iff e₁ then e₂ else e₃, t => do
     checkOutsideIn Γ e₁ $ typ bool
@@ -20,56 +21,61 @@ partial def checkOutsideIn (Γ : Ctx) : Exp → Typ → M Unit
     checkOutsideIn Γ e₃ t
 
   | e, expected => do
-    let found ← inferOutsideIn Γ e
+    let found ← inferOutsideIn Γ [] e
     if expected == found then () else throw s!"type expected '{expected}'; found '{found}'"
 
-partial def inferOutsideIn (Γ : Ctx) : Exp → M Typ
-  | exp let x₁ ∷ t₂ ≔ e₃; e₄ => do
+partial def inferOutsideIn (Γ : Ctx) : App → Exp → M Typ
+  | Ψ, exp let x₁ ∷ t₂ ≔ e₃; e₄ => do
     checkOutsideIn Γ e₃ t₂
-    inferOutsideIn (Γ.push (x₁, t₂)) e₄
+    inferOutsideIn ((x₁, t₂) :: Γ) Ψ e₄
 
-  | e@(exp #x₁) =>
+  | _, e@(exp #x₁) =>
     match Γ.find? (·.1 == x₁) with
     | some (_, t₁) => t₁
     | none         => throw s!"unknown variable '{e}'"
 
-  | e@(exp abs x₁ ⇒ e₂) => do
+  | [], e@(exp abs x₁ ⇒ e₂) => do
     let (t₂, Γ) ← inferInsideOut Γ e₂
     match Γ.find? (·.1 == x₁) with
     | some (_, t₁) => typ t₁ ⇒ t₂
     | none         => throw s!"partially failed to infer '{e} ∷ ? ⇒ {t₂}'"
 
-  | exp e₁ ◁ e₂ => do
-    let t₁ ← inferOutsideIn Γ e₁
+  | t₁ :: Ψ, e@(exp abs x₁ ⇒ e₂) => do
+    let t₂ ← inferOutsideIn ((x₁, t₁) :: Γ) Ψ e₂
+    typ t₁ ⇒ t₂
+
+  | Ψ, exp e₁ ◁ e₂ => do
+    let t₂ ← inferOutsideIn Γ [] e₂
+    let t₁ ← inferOutsideIn Γ (t₂ :: Ψ) e₁
     match t₁ with
-    | typ t₁₁ ⇒ t₁₂ =>
-      checkOutsideIn Γ e₂ t₁₁
-      t₁₂
-    | found => throw s!"type expected '_ ⇒ _'; found '{found}'"
+    | typ _ ⇒ t₁₂ => t₁₂
+    | found       => throw s!"type expected '_ ⇒ _'; found '{found}'"
 
-  | exp ff => typ bool
+  | [], exp ff => typ bool
 
-  | exp tt => typ bool
+  | [], exp tt => typ bool
 
-  | exp iff e₁ then e₂ else e₃ => do
+  | Ψ, exp iff e₁ then e₂ else e₃ => do
     checkOutsideIn Γ e₁ $ typ bool
-    let t₂ ← inferOutsideIn Γ e₂
+    let t₂ ← inferOutsideIn Γ Ψ e₂
     checkOutsideIn Γ e₃ t₂
     t₂
 
-  | exp e₁ ∷ t₂ => do
+  | _, exp e₁ ∷ t₂ => do
     checkOutsideIn Γ e₁ t₂
     t₂
+
+  | _, e => throw s!"failed to infer '{e}'"
 
 partial def checkInsideOut (Γ : Ctx) : Exp → Typ → M Ctx
   | exp let x₁ ∷ t₂ ≔ e₃; e₄, t => do
     let Γ ← checkInsideOut Γ e₃ t₂
-    checkInsideOut (Γ.push (x₁, t₂)) e₄ t
+    checkInsideOut ((x₁, t₂) :: Γ) e₄ t
 
-  | exp #x₁, t => Γ.push (x₁, t)
+  | exp #x₁, t => (x₁, t) :: Γ
 
   | exp abs x₁ ⇒ e₂, typ t₁ ⇒ t₂ => do
-    checkInsideOut (Γ.push (x₁, t₁)) e₂ t₂
+    checkInsideOut ((x₁, t₁) :: Γ) e₂ t₂
 
   | exp iff e₁ then e₂ else e₃, t => do
     let Γ ← checkInsideOut Γ e₁ $ typ bool
@@ -83,7 +89,7 @@ partial def checkInsideOut (Γ : Ctx) : Exp → Typ → M Ctx
 partial def inferInsideOut (Γ : Ctx) : Exp → M (Typ × Ctx)
   | exp let x₁ ∷ t₂ ≔ e₃; e₄ => do
     let Γ ← checkInsideOut Γ e₃ t₂
-    inferInsideOut (Γ.push (x₁, t₂)) e₄
+    inferInsideOut ((x₁, t₂) :: Γ) e₄
 
   | e@(exp #x₁) =>
     match Γ.find? (·.1 == x₁) with
--- InsideOut/Example.lean
+++ InsideOut/Example.lean
@@ -1,37 +1,51 @@
 import InsideOut.Elaborate
 
+notation Γ " | " Ψ " ⊢ " e => inferOutsideIn Γ Ψ e
+
 #eval IO.println "-- ok"
 
-#eval inferOutsideIn #[] $ exp ff
+#eval [] | [] ⊢ exp ff
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ≔ abs "x" ⇒ #"x";
   abs "x" ⇒ #"f" ◁ #"x"
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
   abs "x" ⇒ #"f" ◁ #"x"
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"y";
   abs "x" ⇒ #"f" ◁ #"x"
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
   abs "x" ⇒ abs "y" ⇒ #"f" ◁ #"x" ◁ #"y"
 
+#eval [] | [] ⊢ exp (abs "x" ⇒ #"x") ◁ ff
 
+#eval [] | [] ⊢ exp (abs "x" ⇒ ff) ◁ ff
+
+#eval [] | [] ⊢ exp (iff ff then abs "x" ⇒ #"x" else abs "x" ⇒ #"x") ◁ ff
+
+#eval [] | [] ⊢ exp
+  let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
+  (abs "x" ⇒ #"f") ◁ ff
+
+#eval [] | [] ⊢ exp
+  let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
+  (abs "x" ⇒ abs "y" ⇒ #"f") ◁ ff ◁ ff
 
 #eval IO.println "-- error"
 
-#eval inferOutsideIn #[] $ exp abs "x" ⇒ #"x"
+#eval [] | [] ⊢ exp abs "x" ⇒ #"x"
 
-#eval inferOutsideIn #[] $ exp abs "x" ⇒ ff
+#eval [] | [] ⊢ exp abs "x" ⇒ ff
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
   abs "x" ⇒ #"f" ◁ ff
 
-#eval inferOutsideIn #[] $ exp
+#eval [] | [] ⊢ exp
   let "f" ∷ bool ⇒ bool ⇒ bool ≔ abs "x" ⇒ abs "y" ⇒ #"x";
   abs "x" ⇒ abs "y" ⇒ #"f" ◁ #"x" ◁ #"x"
